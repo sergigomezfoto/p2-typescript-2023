030801decee3fe800cf3e458acf2cb4ae9c1a9cb
Sergi Gómez Garcia <sergi@sergigomez.com>
2023-04-17 22:01:43 +0200

    c2

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 const fetchJson = async () => {
     const result = await fetch('https://www.freetogame.com/api/games');
     const jsonResponse :Array<any> = await result.json();
-     console.log(jsonResponse.length); 
+     console.log(jsonResponse); 
 
 }
 
