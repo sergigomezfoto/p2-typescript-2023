18e2b0d64328185d6b8616599c1fdc1259bf7bd8
Sergi Gómez Garcia <sergi@sergigomez.com>
2023-04-18 21:45:06 +0200

    18/04

--- fetch.ts
+++ fetch.ts
@@ -1,7 +1,8 @@
 
 
 
-class DataItem {
+export class DataItem {
+
     constructor(
         public images: string,
         public title: string,
@@ -12,15 +13,36 @@ class DataItem {
         public keywords: string[]
     ) { }
 
+    get ellipsisTitle() {
+        let limit: number = 45;
+        if (this.title.length <= limit) {
+            return this.title
+        }
+        return this.title.slice(0, (limit - 3)) + '...'
+    }
+    get mediumImage() {
+        return this.image.replace(/~thumb\./, '~medium\.')
+    }
+    get originalImage() {
+        return this.image.replace(/~thumb\./, '~orig\.')
+    }
+    get keywordsString(){
+        return this.keywords.join(' · ')
+    }
 }
 
 
-export const fetchJson = async () => {
-    const data = await fetch("https://images-api.nasa.gov/search?q=nebula&media_type=image");
-    const jsonResponse: any = await data.json();
-    const cleanData: Array<DataItem> = [];
-    for (const { href, data, links } of jsonResponse.collection.items) {
-        cleanData.push(new DataItem(href, data[0].title, links[0].href, data[0].center, data[0].date_created, data[0].description, data[0].keywords));
-    };
-    return cleanData
+export const fetchJson = async (query: string = 'nebula') => {
+    let cleanData: Array<DataItem> | string = [];
+    try {
+        const data = await fetch(`https://images-api.naa.gov/search?q=${query}&media_type=image`);
+        const jsonResponse: any = await data.json();
+        for (const { href, data, links } of jsonResponse.collection.items) {
+            cleanData.push(new DataItem(href, data[0].title, links[0].href, data[0].center, data[0].date_created, data[0].description, data[0].keywords));
+        };
+        return cleanData
+        
+    } catch (error) {
+       console.log(error);
+    }
 }
--- main.ts
+++ main.ts
@@ -1,6 +1,46 @@
-import { fetchJson } from "./fetch.js";
+import { DataItem, fetchJson } from "./fetch.js";
 import { writeFile } from "fs/promises";
-const resultdata =  JSON.stringify(await fetchJson());
+const resultdata = JSON.stringify(await fetchJson());
 
-await writeFile('results.json', resultdata);
 
+
+
+//await writeFile('results.json', resultdata);
+
+
+// *********************************************************************************************************************
+//test
+
+// let testobj :any= {
+//     href: "https://images-assets.nasa.gov/image/PIA15817/collection.json",
+//     data: [
+//       {
+//         center: "JPL",
+//         title: "The Helix Nebula: Unraveling at the Seams",
+//         nasa_id: "PIA15817",
+//         date_created: "2012-10-03T21:30:49Z",
+//         "keywords": [
+//             "Hubble Space Telescope",
+//             "HST",
+//             "Advanced Camera for Surveys",
+//             "ACS",
+//             "Omega Nebula",
+//             "M17",
+//             "Swan Nebula"
+//         ],
+//         media_type: "image",
+//         description_508: "This image from NASA Spitzer and GALEX shows the Helix nebula, a dying star throwing a cosmic tantrum. In death, the star dusty outer layers are unraveling into space, glowing from the intense UV radiation being pumped out by the hot stellar core.",
+//         secondary_creator: "NASA/JPL-Caltech",
+//         description: "This image from NASA Spitzer and GALEX shows the Helix nebula, a dying star throwing a cosmic tantrum. In death, the star dusty outer layers are unraveling into space, glowing from the intense UV radiation being pumped out by the hot stellar core."
+//       }
+//     ],
+//     links: [
+//       {
+//         href: "https://images-assets.nasa.gov/image/PIA15817/PIA15817~thumb.jpg",
+//         rel: "preview",
+//         render: "image"
+//       }
+//     ]
+//   }
+// let tst= new DataItem(testobj.href, testobj.data[0].title, testobj.links[0].href, testobj.data[0].center, testobj.data[0].date_created, testobj.data[0].description, testobj.data[0].keywords);
+// console.log(tst.ellipsisTitle);
